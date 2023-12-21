41b80021320e5f910af46c0999b3431e4e698690
RaST-EDITH <raghvsinghtomar1975@gmail.com>
2023-12-21 10:04:11 +0530

    Highilighting face

--- versa.py
+++ versa.py
@@ -10,3 +10,13 @@ def checkFace( img ) :
     faceCascade = cv2.CascadeClassifier(r"C:\Users\ASUS\haarcascade_frontalface_default.xml")
     face_rtg = faceCascade.detectMultiScale(gray_img, 1.3, 9)
     # print( face_rtg )
+
+    for (x, y, w, h) in face_rtg:
+        cv2.rectangle(img, (x-10, y-25), (x+w+6, y+h+20), ( 0, 255, 0), 2)
+
+    imgRoi = img[y:y+h,x:x+w]
+
+    cv2.imshow('Detected face', img)
+    # cv2.imshow('Face', imgRoi)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
